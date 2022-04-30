36de532d307e4c4140d5304be66141ffd4335f8e
Gleb Sukristik <gleb15a@gmail.com>
2022-04-30 14:56:10 +0300

    Initial Commit

---
+++ SerializatorApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SerializatorApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
